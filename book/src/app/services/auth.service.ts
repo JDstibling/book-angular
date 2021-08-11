135475c3ec618c5da4e5582c3a6fc1dfb190720e
jd <jean-didier@kinomap.net>
2021-08-11 12:42:14 +0200

    auth with firebase

--- book/src/app/services/auth.service.ts
+++ book/src/app/services/auth.service.ts
@@ -1,3 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+
+
+
+//import { Promise } from 'dns';
+//import * as firebase from 'firebase';
+
+@Injectable()
 export class AuthService {
+
+    constructor() {}
+
+    createNewUser(email: string, password: string) {
+        return new Promise<void>(
+          (resolve, reject) => {
+            firebase.auth().createUserWithEmailAndPassword(email, password).then(
+              () => {
+                resolve();
+              },
+              (error) => {
+                reject(error);
+              }
+            );
+          }
+        );
+    }
     
 }
