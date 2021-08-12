5ee71bbc1b76a8e261571f7453b43a1730222fe5
jd <jean-didier@kinomap.net>
2021-08-12 12:56:45 +0200

    ts booklist

--- book/src/app/book-list/book-list.component.ts
+++ book/src/app/book-list/book-list.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Book } from '../models/Book.model';
+import { BookService } from '../services/books.service';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubscription: Subscription = new Subscription;
+
+  constructor(private booksService: BookService, private Router: Router) { }
 
   ngOnInit(): void {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
+  }
+
+  onNewBook() {
+    this.Router.navigate(['/books', 'new']);
+
+  }
+
+  onDeletebook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id : number) {
+    this.Router.navigate(['/books', 'view', id]);
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
   }
 
 }
